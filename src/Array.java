48b1ef673498e657b703ee0f5d8e430c58026cb0
Shivangi-1203 <shivangi.kumari@stackmbiz.com>
2022-01-31 16:22:16 +0530

    Adding a new line

--- src/Array.java
+++ src/Array.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Array {
     public static void main(String[] args) {
-        System.out.println("Give input");
+        System.out.println("Input size");
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] numbers = new int[n];
@@ -11,11 +11,11 @@ public class Array {
             numbers[i] = sc.nextInt();
         }
 
-        int x = sc.nextInt();
+        int searchingNumber = sc.nextInt();
 
         for (int i = 0; i < n; i++) {
-            if (numbers[i] == x) {
-                System.out.println("x found at index: " + i);
+            if (numbers[i] == searchingNumber) {
+                System.out.println("searchingNumber found at index: " + i);
             }
         }
     }
